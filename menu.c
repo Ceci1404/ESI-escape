33693464cbaf877aa5646e06523f8d203f2a8cca
F-R-A-Nj <theloge07@gmail.com>
2026-03-09 17:06:04 +0100

    menu.c

--- menu.c
+++ menu.c
@@ -1,1 +1,33 @@
-#include<stdio.h>
+#include"menu.h"
+int main(){
+int x=0;
+
+do{
+    printf("        --ESI SCAPE--     \n");
+    printf("----------\n");
+    printf("1. Nueva partida.");
+    printf("2. Cargar partida."); 
+    printf("3. Salir.");
+if(x<0 || x>3){
+    printf("OPCION INCORRECTA,PORFAVOR ELIJA CORRECTAMENTE.\n");
+}
+} while(x<0 || x>3);
+switch(x){
+    case 1{
+
+        break;
+    }
+    case 2{
+
+        break;
+    }
+        case 3{
+
+        break;
+    }
+
+}
+
+return 0;
+}
+
