b267ca01927a4ef4657c36c5b27b977b80e77666
F-R-A-Nj <theloge07@gmail.com>
2026-03-09 17:32:54 +0100

    menu.c

--- menu.c
+++ menu.c
@@ -1,33 +1,103 @@
 #include"menu.h"
+void menu(int x);
+void partida(int p);
+
 int main(){
 int x=0;
+int p=0;
+menu(x);
+partida(p);
+return 0;
+}
 
+void menu(int x){
 do{
     printf("        --ESI SCAPE--     \n");
     printf("----------\n");
-    printf("1. Nueva partida.");
-    printf("2. Cargar partida."); 
-    printf("3. Salir.");
-if(x<0 || x>3){
+    printf("1. Nueva partida.\n");
+    printf("2. Cargar partida.\n"); 
+    printf("3. Salir.\n");
+    scanf("%d",&x);
+if(x<1 || x>3){
     printf("OPCION INCORRECTA,PORFAVOR ELIJA CORRECTAMENTE.\n");
 }
-} while(x<0 || x>3);
+} while(x<1 || x>3);
 switch(x){
-    case 1{
+    case 1:
 
         break;
-    }
-    case 2{
+    
+    case 2:
 
         break;
-    }
-        case 3{
+    
+        case 3:
 
         break;
-    }
+    
 
+} 
 }
 
-return 0;
+void partida(int p){
+do{
+    printf("Sala: %s\n");//HABRIA QUE ACCEDER A LA SALA ACTUAL DEL JUGADOR
+    putchar("\n");
+    
+    printf("1. Describir sala.\n");
+    printf("2. Examinar (objetos y salidas).\n"); 
+    printf("3. Entrar en otra sala.\n");
+    printf("4. Coger objeto.\n");
+    printf("5. Soltar objeto.\n"); 
+    printf("6. Inventario.\n");
+    printf("7. Usar objeto.\n");
+    printf("8. Resolver puzle / introducir código.\n"); 
+    printf("9. Guardar partida.\n");
+    printf("10. Volver.\n");
+
+        scanf("%d",&p);
+if(p<1 || p>10){
+    printf("OPCION INCORRECTA,PORFAVOR ELIJA CORRECTAMENTE.\n");
+}
+} while(p<1 || p>10);
+
+switch(p){
+    case 1:
+
+        break;
+    
+    case 2:
+
+        break;
+    
+    case 3:
+
+        break;
+        
+    case 4:
+
+        break;
+    case 5:
+
+        break;
+    case 6:
+
+        break;
+        
+    case 7:
+
+        break;
+    case 8:
+
+        break;
+    case 9:
+
+        break;
+    case 10:
+
+        break;
+    
+
+} 
 }
 
