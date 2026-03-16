4f9bc5500902a2d84fa4c89eef05d8f1da5a740f
F-R-A-Nj <theloge07@gmail.com>
2026-03-16 17:59:33 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,96 +1,107 @@
-#include"menu.h"
-void menu(int x);
-void partida(int p);
+#include <stdio.h>
+#include <stdlib.h>
+#include "menu.h"
 
-void menu(int x){
-do{
-    printf("        --ESI SCAPE--     \n");
-    printf("        ----------\n");
-    printf("1. Nueva partida.\n");
-    printf("2. Cargar partida.\n"); 
-    printf("3. Salir.\n");
-    scanf("%d",&x);
-if(x<1 || x>3){
-    printf("OPCION INCORRECTA,PORFAVOR ELIJA CORRECTAMENTE.\n");
+void menu_principal();
+void menu_juego(partida *p_actual);
+
+int main() {
+    menu_principal();
+    return 0;
 }
-} while(x<1 || x>3);
-switch(x){
-    case 1:
 
-        break;
-    
-    case 2:
+void menu_principal() {
+    int x = 0;
+    //Creamos la variable que almacenará todo eljuego
+    partida mi_partida; 
 
-        break;
-    
+    do {
+        printf("       --ESI SCAPE--     \n");
+        printf("        ----------\n");
+        printf("1. Nueva partida.\n");
+        printf("2. Cargar partida.\n"); 
+        printf("3. Salir.\n");
+        printf("Elige una opcion: ");
+        scanf("%d", &x);
+
+        if(x < 1 || x > 3){
+            printf("OPCION INCORRECTA, POR FAVOR ELIJA CORRECTAMENTE.\n");
+        }
+    } while(x < 1 || x > 3);
+
+    switch(x) {
+        case 1:
+            printf("\n[CREANDO NUEVA PARTIDA...]\n");
+        
+            menu_juego(&mi_partida); // Entra a jugar pasándole los datos
+            break;
+        
+        case 2:
+            printf("\n[CARGANDO PARTIDA...]\n");
+            // llamamos a  función de leer el txt
+           
+            menu_juego(&mi_partida); 
+            break;
+        
         case 3:
-
-        break;
-    
-
-} 
+            printf("\nSaliendo del juego... ¡Hasta pronto!\n");
+            break;
+    } 
 }
 
-void partida(int p){
-do{
-    printf("   --Sala: %s--\n");//HABRIA QUE ACCEDER A LA SALA ACTUAL DEL JUGADOR
-    putchar("\n");
-    
-    printf("1. Describir sala.\n");
-    printf("2. Examinar (objetos y salidas).\n"); 
-    printf("3. Entrar en otra sala.\n");
-    printf("4. Coger objeto.\n");
-    printf("5. Soltar objeto.\n"); 
-    printf("6. Inventario.\n");
-    printf("7. Usar objeto.\n");
-    printf("8. Resolver puzle / introducir código.\n"); 
-    printf("9. Guardar partida.\n");
-    printf("10. Volver.\n");
 
-        scanf("%d",&p);
-if(p<1 || p>10){
-    printf("OPCION INCORRECTA,PORFAVOR ELIJA CORRECTAMENTE.\n");
-}
-} while(p<1 || p>10);
+void menu_juego(partida *p_actual) { // recibe el estado completo del juego
+    int p = 0;
 
-switch(p){
-    case 1:
-
-        break;
-    
-    case 2:
-
-        break;
-    
-    case 3:
-
-        break;
+    do {
+        // imprimimos sala actual usando la estructura
+        printf("\n   --Sala: %s--\n", p_actual->sala_actual); 
         
-    case 4:
+        printf("1. Describir sala.\n");
+        printf("2. Examinar (objetos y salidas).\n"); 
+        printf("3. Entrar en otra sala.\n");
+        printf("4. Coger objeto.\n");
+        printf("5. Soltar objeto.\n"); 
+        printf("6. Inventario.\n");
+        printf("7. Usar objeto.\n");
+        printf("8. Resolver puzle / introducir codigo.\n"); 
+        printf("9. Guardar partida.\n");
+        printf("10. Volver.\n");
+        printf("Elige una accion: ");
 
-        break;
-    case 5:
+        scanf("%d", &p);
 
-        break;
-    case 6:
-
-        break;
-        
-    case 7:
-
-        break;
-    case 8:
-
-        break;
-    case 9:
-
-        break;
-    case 10:
-
-        break;
-    
-
-} 
+        if(p < 1 || p > 10){
+            printf("OPCION INCORRECTA, POR FAVOR ELIJA CORRECTAMENTE.\n");
+        } else {
+            
+            switch(p) {
+            case 1:
+                    printf("Describiendo la sala...\n");
+                    break;
+            case 2:
+                    break;
+             case 3: // Entrar en otra sala
+                    
+                    break;
+             case 4:
+                    break;
+             case 5:
+                    break;
+            case 6:
+                    break;
+            case 7:
+                    break;
+            case 8:
+                    break;
+            case 9:
+                    printf("Guardando la partida actual...\n");
+                    // se debe llamar a otra funcion
+                    break;
+            case 10:
+                    printf("Volviendo al menu principal...\n");
+                    return; 
+            }
+        }
+    } while(p <1|| p>10);
 }
-
-
