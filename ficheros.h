004847e13f407a06d3518dc91330835c7395ad57
mariosenriquez-beep <mariosenriquez@gmail.com>
2026-03-16 17:08:03 +0100

    Clean up includes in ficheros.h
    Removed unnecessary includes from ficheros.h

--- ficheros.h
+++ ficheros.h
@@ -1,9 +1,6 @@
 #ifndef FICHEROS_H
 #define FICHEROS_H
 //LECTURA Y ESCRITURA DE FICHEROS
-#include "librerias.h"
-#include "partida.h"
-#include "jugadores.h"
 
     void leerficheros ();
     void cargarficheros();
