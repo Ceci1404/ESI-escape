586f73b4ac05f2f33f8c431ec889244e65fd6dc7
sus <sufesi790@gmail.com>
2026-03-20 20:18:15 +0100

    ubicacion.c de Ceci

--- ubicacion.c
+++ ubicacion.c
@@ -6,19 +6,20 @@
 //Cabecera: void entrarsala(partida p, sala s, conexion co, conexion *econ, int ncon)
 //Precondición: La función recive la estructura de partida, conexion y sala y el vector de conexiones inicializados
 //Postcondición: El jugador se movera entre salas si es posible
-void entrarsala(partida p, conexion co, sala s, conexion *econ, int ncon){
-    int entrar,i=0;
-    strcpy(c,p.sala_actual);
-    while (i!=ncon){
-        if (strcmp(co.id_conexion,p.sala_actual)==0){   //Si el id de la conexion coincide con el id de la sala actual del jugador, se comprueba si la conexion esta activa o bloqueada))
-            if (co.estado[i]==TRUE){
+void entrarsala(partida p, conexion* co, sala s, conexion *econ, int ncon){
+    int entrar;
+    strcpy(co,p.sala_actual);
+    for (int i=0; i<=ncon; i++){
+        if (strcmp(co[i].id_conexion,p.sala_actual)==0){   //Si el id de la conexion coincide con el id de la sala actual del jugador, se comprueba si la conexion esta activa o bloqueada))
+            if (co[i].estado==true){
                 do {
-                    printf("Puedes entrar a la sala: %s, ¿quieres entrar? Si:1 No:0 \n", s.nomb_sala[co.id_destino]);
-                    scanf("%d", &entrar)
+                    printf("Puedes entrar a la sala: %s, ¿quieres entrar? Si:1 No:0 \n", s.nomb_sala[co.id_destino]); 
+                    ///comentario de susi: esto no funciona porque, en cualquier caso seria s[x].nomb_sala, ya que s es un vector de estructuras y luego el id_destino no es un int así que no podria acceder a esa posición 
+                    scanf("%d", &entrar);
                 }while (entrar!=1 && entrar!=0);
-                if(entrar==1) strcpy(co.id_destino,p.sala_actual);      //Si la conexión esta activa, se copia el id de la sala a la que se puede entrar en la variable en la sala actual
+                if(entrar==1) strcpy(co[i].id_destino,p.sala_actual);      //Si la conexión esta activa, se copia el id de la sala a la que se puede entrar en la variable en la sala actual
             }
-            else printf("La conexión de la sala esta bloqueada, solucion: %s",co.cond);
-        i++;}
+            else printf("La conexión de la sala esta bloqueada, solucion: %s",co[i].cond);
+}
 }
 }
