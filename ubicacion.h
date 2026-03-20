3815980c9e5961e37b43210ac73ab7369e35b7cd
Ceci <162624908+Ceci1404@users.noreply.github.com>
2026-03-20 17:57:39 +0100

    Add files via upload

---
+++ ubicacion.h
@@ -0,0 +1,12 @@
+/*En este modulo el jugador se mueve de ubicaciones*/
+#ifndef UBICACION_H
+#define UBICACION_H
+#include "librerias.h"
+
+
+//Cabecera: void entrarsala(partida , sala )
+//Precondición: La función recive la estructura de partida, conexion y sala
+//Postcondición: El jugador se movera entre salas si es posible
+void entrarsala(partidas , conexion );
+
+#endif
