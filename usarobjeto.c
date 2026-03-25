89b8a6bc6c46cd6ccc74fa5b4afab118c8237f63
Ceci <162624908+Ceci1404@users.noreply.github.com>
2026-03-25 23:45:46 +0100

    Add files via upload

---
+++ usarobjeto.c
@@ -0,0 +1,37 @@
+#include "usarobjeto.h"
+#include "partida.h"
+#include "conexiones.h"
+#include "jugadores.h"
+#include "objetos.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Cabecera: void usarobjeto(objetos o,partida p, conexiones c, jugador j)
+//Precondición: La función recive la estructura de objetos, partida, conexiones y jugador inicializada
+//Poscondición: El jugador podra usar un objeto del inventario para desbloquear conexiones
+void usarobjeto(objetos o,partida p, conexiones c, jugador j{
+    int usar, i;
+    int x,y,z;
+    for (int i=0; i<=ncon; i++){
+        if (strcmp(co[i].id_conexion,p.sala_actual)==0){
+            for(y=0; y<=j[/**/].cant_obj; y++){
+                if (strcmp(co[i].cond,j[/**/].inventario[y].objinv)==0){
+                    for(z=0;z<=nobj; z++){
+                        if (strcmp(o[z].id_obj,j[/**/].inventario[y].objinv)==0)
+                    }
+                    do{
+                        printf("Puedes usar el objeto %s en la sala\n ¿quieres usarlo? Si:1 No:0 \n", o[z].nomb_obj);
+                    }while (usar!=1 && usar!=0);
+
+                    if (usar == 1){
+                        p.num_conexunlocked++;);
+                        realloc(p.conex_desbloqueadas, p.num_conexunlocked * sizeof(conexunlocked));
+                        strcpy(co[i].id_destino, p.conex_desbloqueadas[p.num_conexunlocked-1].id_conexion);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ usarobjeto.h
@@ -0,0 +1,10 @@
+#ifndef USAROBJETO_H
+#define USAROBJETO_H
+#include "librerias.h"
+
+//Cabecera: void usarobjeto(objetos ,partida , conexiones , jugador )
+//Precondición: La función recive la estructura de objetos, partida, conexiones y jugador inicializada
+//Poscondición: El jugador podra usar un objeto del inventario para desbloquear conexiones
+void usarobjeto(objetos ,partida , conexiones , jugador );
+
+#endif
