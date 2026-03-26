231be642ccaa8c419b9bac86ea70bd65bf9c1d96
Ceci <162624908+Ceci1404@users.noreply.github.com>
2026-03-26 01:45:40 +0100

    Add files via upload

---
+++ inventario.c
@@ -0,0 +1,25 @@
+#include "soltarobjeto.h"
+#include "partida.h"
+#include "jugadores.h"
+#include "objetos.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+//Cabecera: void soltarobjeto(jugadores j, objetos o, partida p)
+//Precondición: La funcion recive la estructura de jugadores, objetos y partida inicializada
+//Postcondición: El jugador mostrara los objetos que tiene en su inventario
+void mostrarinventario(jugadores j, objetos o, partida p){
+    int x,y;
+    printf("Los objetos en tu inventario son:\n");
+    for(x=0; x<=ninv; x++){
+        for(y=0; y<=j[/* indice del jugador */].cant_obj; y++){
+            if(j[/* indice del jugador */].inventario[y].objinv == o[x].id_obj){
+                printf("%d %s %s %s %s \n", x, o[x].id_obj, o[x].nomb_obj, o[x].desc, o[x].localiz);
+
+            }
+        }
+    }
+}
---
+++ inventario.h
@@ -0,0 +1,10 @@
+#ifndef INVENTARIO_H
+#define INVENTARIO_H
+
+
+//Cabecera: void soltarobjeto(jugadores , objetos , partida )
+//Precondición: La funcion recive la estructura de jugadores, objetos y partida inicializada
+//Postcondición: El jugador mostrara los objetos que tiene en su inventario
+void mostrarinventario(jugadores , objetos , partida );
+
+#endif
