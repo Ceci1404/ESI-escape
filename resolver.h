80d2be778a1b15ac08023ee961d2923640ad7c65
Ceci <162624908+Ceci1404@users.noreply.github.com>
2026-04-01 17:38:48 +0200

    Add files via upload

---
+++ resolver.h
@@ -0,0 +1,10 @@
+#ifndef RESOLVER_H
+#define RESOLVER_H
+
+
+//Cabecera: void resolver(puzles puz, partida p, int npuz)
+//Precondición: La función recive la estructura de puzles y partida inicializada
+//Postcondición: El jugador podra resolver puzles
+void resolver(puzles puz, partida p, int npuz);
+
+#endif
