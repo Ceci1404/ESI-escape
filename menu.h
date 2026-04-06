7456f258288494bf4a0aa9abb52f12abed543bee
F-R-A-Nj <theloge07@gmail.com>
2026-04-06 16:25:14 +0200

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,13 +1,24 @@
-#ifndef MENU_H
-#define MENU_H
-
-#include "conexiones.h"
-#include "puzles.h"
-#include "jugadores.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "menu.h"
 #include "partida.h"
 #include "salas.h"
+#include "conexiones.h"
+#include "jugadores.h"
+#include "objetos.h"
+#include "puzles.h"
+#include "ficheros.h"
+#include "describir.h"
+#include "examinar.h"
+#include "ubicacion.h"
+#include "cogerobjeto.h"
+#include "soltarobjeto.h"
+#include "inventario.h"
+#include "usarobjeto.h"
+#include "resolver.h"
 
-void menu(int x);
-void partida (int p);
+void menu_principal();
+void menu_juego(partida *p_actual, sala *v_salas, conexion *v_conex, jugador *v_jug, objetos *v_obj, puzle *v_puz, int nsal, int nobj, int ncon, int npuz);
 
 #endif
