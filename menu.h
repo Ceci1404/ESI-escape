80582b2b9ec667b35109fa97544b3a674eb5c256
F-R-A-Nj <theloge07@gmail.com>
2026-04-12 18:44:50 +0200

    Update menu.h

--- menu.h
+++ menu.h
@@ -8,12 +8,10 @@
 void menu_principal();
 
 // Gestiona la creación de un nuevo jugador y el inicio desde cero
-void crear_nueva_partida(partidas *p, sala *v_salas, conexion *v_conex, jugador *v_jug, objetos *v_obj, puzle *v_puz, int nsal, int nobj, int ncon, int npuz, partidas *v_partidas, int total_jugadores, int total_partidas);
-
+void crear_nueva_partida(partida *p_actual, jugador *v_jug, int *total_jugadores);
 // Gestiona la carga de datos del disco duro para restaurar el progreso
-void cargar_partida_existente(partidas *p, sala *v_salas, conexion *v_conex, jugador *v_jug, objetos *v_obj, puzle *v_puz, int nsal, int nobj, int ncon, int npuz, partidas *v_partidas, int total_jugadores, int total_partidas);
-
+int cargar_partida_existente(partida *p_actual, jugador *v_jug, int total_jugadores, partidas *v_partidas, int total_partidas);
 // Bucle principal donde el jugador decide qué acciones tomar dentro de la sala
-void menu_juego(partidas *p_actual, sala *v_salas, conexion *v_conex, jugador *v_jug, objetos *v_obj, puzle *v_puz, int nsal, int nobj, int ncon, int npuz, partidas *v_partidas, int total_jugadores, int total_partidas);
+void menu_juego(partida *p_actual, sala *v_salas, conexion *v_conex, jugador *v_jug, objetos *v_obj, puzle *v_puz, int nsal, int nobj, int ncon, int npuz, partidas *v_partidas, int total_jugadores, int total_partidas);
 
 #endif // MENU_H
