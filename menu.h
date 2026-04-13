64ea05a8027c35be3def7e4784eb1521114cde50
F-R-A-Nj <theloge07@gmail.com>
2026-04-13 16:32:23 +0200

    Update menu.h

--- menu.h
+++ menu.h
@@ -10,11 +10,10 @@ void menu_principal(partidas *, sala *, conexion *, jugador *, objetos *, puzle 
 // Bucle principal donde el jugador decide qué acciones tomar dentro de la sala
 void menu_juego(partidas *, sala *, conexion *, jugador *, objetos *, puzle *, int *, int *, int*, int *, int *, int *, int *);
 
-/*
+
 // Gestiona la creación de un nuevo jugador y el inicio desde cero
 void crear_nueva_partida(partidas *, jugador *, int *);*/
 // Gestiona la carga de datos del disco duro para restaurar el progreso
-/*int cargar_partida_existente(partidas *, jugador *, int *, int *);
-*/
+int cargar_partida_existente(partidas *, jugador *, int *, int *);
 
 #endif // MENU_H
