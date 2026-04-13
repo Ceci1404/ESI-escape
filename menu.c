e106d6d397c1f83bbaa1e28f0a0ac6ee3304a6c7
Ceci <162624908+Ceci1404@users.noreply.github.com>
2026-04-13 13:47:57 +0200

    Add files via upload

--- menu.c
+++ menu.c
@@ -11,43 +11,16 @@
 #include "objetos.h"
 #include "puzles.h"
 
+
 // --- CABECERAS DE MÓDULOS DE  COMPAÑEROS ---
 #include "ficheros.h"
-#include "describir.h"
-#include "examinar.h"
-#include "ubicacion.h"
-#include "cogerobjeto.h"
-#include "soltarobjeto.h"
-#include "inventario.h"
-#include "usarobjeto.h"
-#include "resolver.h"
-
-
-// --- MAIN ---
-int main() {
-    menu_principal(); 
-    return 0;
-}
+#include "logica.h"
+#include "juego.h"
 
 
 // --- MENÚ PRINCIPAL ---
 void menu_principal() {
-    int x = 0;
-    partida mi_partida; 
-    
-    // Punteros del juego
-    sala *v_salas = NULL;
-    conexion *v_conex = NULL;
-    jugador *v_jug = NULL;
-    objetos *v_obj = NULL;
-    puzle *v_puz = NULL;
-    partidas *v_partidas = NULL;
-    
-    // Contadores
-    int nsal = 20, nobj = 20, ncon = 20, npuz = 10;
-    int total_jugadores = 1; 
-    int total_partidas = 1;  
-
+    int x;
     do {
         printf("\n       --ESI SCAPE--     \n");
         printf("        ----------\n");
@@ -66,7 +39,7 @@ void menu_principal() {
                     leerficheros(v_salas, v_conex, v_jug, v_obj, v_puz);
                     
                     // 2. Gestionamos solo el registro 
-                    crear_nueva_partida(&mi_partida, v_jug, &total_jugadores);
+                    nuevapartida(jugador *v_jug, partidas *v_partidas,int *jug, int *njug, int *par, int *numpar);
                     
                     // 3. LLAMADAAL JUEGO , Le pasamos la partida creada y todos los vectores del mapa
                     menu_juego(&mi_partida, v_salas, v_conex, v_jug, v_obj, v_puz, nsal, nobj, ncon, npuz, v_partidas, total_jugadores, total_partidas);
@@ -76,12 +49,9 @@ void menu_principal() {
                     // 1. Cargamos el mapa base
                     leerficheros(v_salas, v_conex, v_jug, v_obj, v_puz);
                     
-                    // Si el login devuelve 1 (éxito), entramos al juego
-                    if (cargar_partida_existente(&mi_partida, v_jug, total_jugadores, v_partidas, total_partidas) == 1) {
-                        
-                        // 3. LLAMADA REAL AL JUEGO
-                        menu_juego(&mi_partida, v_salas, v_conex, v_jug, v_obj, v_puz, nsal, nobj, ncon, npuz, v_partidas, total_jugadores, total_partidas);
-                    }
+                    verificarusuario(jugador *v_jug, int *njug, int *jug, partidas *v_partidas, int *numpar, int *par);
+
+                    menu_juego(&mi_partida, v_salas, v_conex, v_jug, v_obj, v_puz, nsal, nobj, ncon, npuz, v_partidas, total_jugadores, total_partidas);
                     break;
 
                 case 3:
@@ -94,12 +64,10 @@ void menu_principal() {
 
 
 // --- MENU DE JUEGO ---
-void menu_juego(partida *p_actual, sala *v_salas, conexion *v_conex, jugador *v_jug, objetos *v_obj, puzle *v_puz, int nsal, int nobj, int ncon, int npuz, partidas *v_partidas, int total_jugadores, int total_partidas) { 
+void menu_juego(partidas *p_actual, sala *v_salas, conexion *v_conex, jugador *v_jug, objetos *v_obj, puzle *v_puz, int *numsal, int *numobj, int *numcon, int *numpuz, int *jug, int *par) { 
     int p = 0;
 
     do {
-        printf("\n   --Sala Actual: %s--\n", p_actual->sala_actual); 
-        
         printf("1. Describir sala.\n");
         printf("2. Examinar (objetos y salidas).\n"); 
         printf("3. Entrar en otra sala.\n");
@@ -119,28 +87,28 @@ void menu_juego(partida *p_actual, sala *v_salas, conexion *v_conex, jugador *v_
         } else {
             switch(p) {
                 case 1:
-                    describir(v_salas, *p_actual); 
+                    describir(v_salas, *p_actual, *numsal, *par); 
                     break;
                 case 2:
-                    examinar(v_salas, v_obj, *p_actual, *v_jug, nsal, nobj, 10, ncon);
+                    examinar(v_salas, v_obj, *p_actual, *v_jug,*v_conex, *numsal, *numobj , *numcon, *jug, *par);
                     break;
                 case 3: 
-                    entrarsala(p_actual, v_conex, v_salas, v_conex, ncon, nsal);
+                    entrarsala(p_actual, v_conex, v_salas, *numcon, *numsal, *par);
                     break;
                 case 4:
                     cogerobjeto(*v_jug, *v_obj, *p_actual);
                     break;
                 case 5:
-                    soltarobjeto(*v_jug, *v_obj, *p_actual);
+                    soltarobjeto(*v_puz, *p_actual, *numpuz, *v_jug, *v_obj, *jug, *par);
                     break;
                 case 6:
-                    mostrarinventario(*v_jug, *v_obj, *p_actual);
+                    mostrarinventario(*v_jug, *v_obj, *p_actual, *jug);
                     break;
                 case 7:
-                    usarobjeto(*v_obj, *p_actual, *v_conex, *v_jug);
+                    usarobjeto(*v_obj, *p_actual, *v_conex, *v_jug, *jug, *numobj, *par);
                     break;
                 case 8:
-                    resolver(*v_puz, *p_actual, npuz);
+                    resolver(*v_puz, *p_actual, *numpuz, *par);
                     break;
                 case 9:
                     printf("Guardando progreso de la partida...\n");
@@ -153,7 +121,7 @@ void menu_juego(partida *p_actual, sala *v_salas, conexion *v_conex, jugador *v_
         }
     } while(p != 10); 
 }
-
+/*
 // --- NUEVA PARTIDA ---
 void crear_nueva_partida(partida *p_actual, jugador *v_jug, int *total_jugadores) {
     printf("\n[CREANDO NUEVA PARTIDA...]\n");
@@ -197,6 +165,6 @@ int cargar_partida_existente(partida *p_actual, jugador *v_jug, int total_jugado
     }
 }
 
-
+*/
 
 
