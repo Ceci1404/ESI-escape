12f57eec1762f982b0619aafcdb2f8f39dc45943
Ceci <162624908+Ceci1404@users.noreply.github.com>
2026-04-13 15:29:56 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -7,10 +7,41 @@
 #include "salas.h"
 
 #include "ficheros.h"
+#include "juego.h"
+#include "logica.h"
 #include "menu.h"
 
 
 
-int main(){
+int main() {
 
+    //Definición de variables necesarias para la inicialización de las funciones
+    int *numsal=0;
+    int *numobj=0;
+    int *numcon=0;
+    int *numpuz=0;
+    int *jug=0;
+    int *par=0;
+    char *cad=NULL;
+
+    sala *s=NULL;
+    conexion *c=NULL;
+    jugador *j=NULL;
+    objetos *o=NULL;
+    puzle *puz=NULL;
+    partidas *p=NULL;
+
+    //Llamada a la inicialización de los vectores a partir de los ficheros
+    s = crearsala(cad,&numsal);
+    c = crearconex(cad,&numcon);
+    j = crearjug(cad,&numjug);
+    o = crearobj(cad,&numobj);
+    puz = crearpuz(cad,&numpuz);
+    p = crearpartida(cad,&numpar);
+
+
+        //Llamada a menú principal para la inicialización del juego.
+    menu_principal(*p, s, c, j, o, puz, &numsal, &numobj, &numcon, &numpuz, &jug, &par);
+
+    return 0;
 }
