540f0f1915c87758010bac5929597c57c3c8c878
Ceci <162624908+Ceci1404@users.noreply.github.com>
2026-04-19 18:15:24 +0200

    Add files via upload

--- menu.h
+++ menu.h
@@ -5,15 +5,16 @@
 // --- PROTOTIPOS DE LAS FUNCIONES DEL MENÚ ---
 
 // Inicia el programa y muestra las opciones de Nueva Partida, Cargar o Salir
-void menu_juego(partidas *, sala *, conexion *, jugador *, objetos *, puzle *, int *, int *, int *, int *, int *, int *);
+void menu_principal(partidas *, sala *, conexion *, jugador *, objetos *, puzle *, int *, int *, int *, int *, int *, int *, int *);
 
 // Bucle principal donde el jugador decide qué acciones tomar dentro de la sala
 void menu_juego(partidas *, sala *, conexion *, jugador *, objetos *, puzle *, int *, int *, int*, int *, int *, int *, int *);
 
-
+/*
 // Gestiona la creación de un nuevo jugador y el inicio desde cero
 void crear_nueva_partida(partidas *, jugador *, int *);*/
 // Gestiona la carga de datos del disco duro para restaurar el progreso
-int cargar_partida_existente(partidas *, jugador *, int *, int *);
+/*int cargar_partida_existente(partidas *, jugador *, int *, int *);
+*/
 
 #endif // MENU_H
