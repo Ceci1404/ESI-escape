b7eb8cc6d7baa5adb03094b941f774fd9192fccb
Mario <mariosenriquez@gmail.com>
2026-04-20 15:38:07 +0200

    Update ficheros.h

--- ficheros.h
+++ ficheros.h
@@ -8,9 +8,14 @@
     //cambiar estas funciones a juego.h
     char* ficherotocad(char* filepath,char* cad);
     void limpiar(char* cad);
-    //SUSANA ^
-    
-    void guardar_datos(jugador *lista_jugadores, int total_jugadores, partidas *p_activa);
+    //SUSANA 
+
+//Cabecera: void guardar_datos(jugador *lista_jugadores, int total_jugadores, partidas *p_activa)
+//Precondición: total_jugadores >= 0. Si total_jugadores > 0, lista_jugadores != NULL. p_activa puede ser NULL o apuntar a una estructura válida.
+//Postcondición: Los datos de los jugadores y de la partida activa se vuelcan en los ficheros "Jugadores.txt" y "Partida.txt". 
+//Si ocurre un error de acceso al disco o no hay partida, se notifica por pantalla sin abortar el programa.
+ void guardar_datos(jugador *lista_jugadores, int total_jugadores, partidas *p_activa);
+
 partidas* login_jugador(jugador *lista_jugadores, int total_jugadores, partidas *lista_partidas, int total_partidas);
 
 #endif
